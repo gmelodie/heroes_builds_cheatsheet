0b0a6cef4167013ed2d8a17dfba67e5b153c4979
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-20 11:07:25 -0300

    Update hereos list

--- hbc.py
+++ hbc.py
@@ -1,17 +1,20 @@
 import sys
 from PyQt5.QtWidgets import *
 import mainWindow
+from heroes_build_scrapper import get_heroes_list
 
 
-class Test(QMainWindow, mainWindow.Ui_MainWindow):
+class Window(QMainWindow, mainWindow.Ui_MainWindow):
 
     def __init__(self):
-        super(Test, self).__init__()
+        super(Window, self).__init__()
         self.setupUi(self)
+        heroes = get_heroes_list()
+        self.heroComboBox.addItems(heroes)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = Test()
+    window = Window()
     window.show()
     sys.exit(app.exec_())
