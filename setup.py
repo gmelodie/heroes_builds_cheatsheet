85001080b23035ce82675537bd4215c90b6c2d5d
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-11-08 10:55:34 -0200

    Remove trailling spaces

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from cx_Freeze import setup, Executable
 from multiprocessing import Queue
 
 # Run file with 'build' command to generate exe in corresponding folder
-# 
+#
 # Has to be built with Python 3.6 or earlier because of bug with cx_Freeze
 
-base = "Win32GUI"    
+base = "Win32GUI"
 
 executables = [Executable("hbc.py", base=base)]
 
 packages = ["idna", "sys", "mainWindow", "multiprocessing"]
 includes = ["PyQt5", "heroes_build_scrapper"]
 options = {
-    'build_exe': {    
+    'build_exe': {
         'packages':packages,
         'includes':includes
-    },    
+    },
 }
 
 setup(
